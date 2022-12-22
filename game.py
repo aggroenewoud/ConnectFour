4b1f0ad73053e294058bdb57c6d3b1875f8b9646
Albert Groenewoud <albert.groenewoud001@fclive.nl>
2022-12-22 14:34:02 +0100

    Add select player function

--- game.py
+++ game.py
@@ -6,5 +6,35 @@ class game:
 
     def __init__(self):
 
-        b = board.board(6, 7)
-        b.print_board()
+        self.board = board.board(6, 7)
+
+        self.game_ongoing = True
+
+        self.players = [
+            None,
+            None
+        ]
+
+        self.select_players()
+
+    def select_players(self):
+
+        options = [
+            player.player,
+            bot.bot
+        ]
+
+        print('Select player1: ')
+        user_choice1 = int(input('1. User / 2.Bot : ')) -1
+
+        self.players[0] = options[user_choice1]()
+
+        print('Select player2: ')
+        user_choice2 = int(input('1. User / 2.Bot : ')) -1
+
+        self.players[1] = options[user_choice2]()
+
+        print(self.players)
+
+        if self.players[0] and self.players[1]:
+            return
