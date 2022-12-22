e526be533b0dd22f09266f918e309ff038d088dc
Albert Groenewoud <albert.groenewoud001@fclive.nl>
2022-12-22 11:44:12 +0100

    Add function for creating a board

--- board.py
+++ board.py
@@ -3,3 +3,19 @@ class board:
     def __init__(self):
 
         pass
+
+    def create_board(self, row, column):
+
+        board = []
+
+        for x in range(row):
+
+            board_column = []
+
+            for y in range(column):
+
+                board_column.append(' ')
+
+            board.append(board_column)
+
+        return board
