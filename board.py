ee05344b0d4d9700b2064734f9c509b5074179d9
Albert Groenewoud <albert.groenewoud001@fclive.nl>
2022-12-22 11:57:15 +0100

    Add function to print board with index

--- board.py
+++ board.py
@@ -19,3 +19,22 @@ class board:
             board.append(board_column)
 
         return board
+
+    def print_board(self, board):
+
+        for x in range(len(board)):
+
+            line = str(x + 1) + ' |'
+
+            for y in range(len(board[0])):
+
+                line += board[x][y] + '|'
+
+            print(line)
+
+        y_index = '   '
+
+        for i in range(len(board[0])):
+            y_index += str(i + 1) + ' '
+
+        print(y_index)
