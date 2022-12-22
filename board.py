a8b6ea88b873117bef8aee3cba76c205310f8f8a
Albert Groenewoud <albert.groenewoud001@fclive.nl>
2022-12-22 14:41:58 +0100

    Change indentation of the board class

--- board.py
+++ board.py
@@ -9,15 +9,10 @@ class board:
         board = []
 
         for x in range(row):
-
             board_column = []
-
             for y in range(column):
-
                 board_column.append(' ')
-
             board.append(board_column)
-
         return board
 
     def print_board(self):
@@ -25,13 +20,9 @@ class board:
         board = self.board
 
         for x in range(len(board)):
-
             line = str(x + 1) + ' |'
-
             for y in range(len(board[0])):
-
                 line += board[x][y] + '|'
-
             print(line)
 
         y_index = '   '
